720246b02f8cc602832fa67b614c4cadde0c7b2f
Speedy3434 <renzo.lopez@tecsup.edu.pe>
2023-03-30 16:41:36 -0500

    Renzo Commit

--- index.js
+++ index.js
@@ -2,21 +2,23 @@ const express = require('express')
 
 const app = express()
 
-app.get('/',(req,res) => {
-    res.sendFile('./templates/index.html', {
+
+app.get('/',(req,res)=>{
+    res.sendFile('./static/index.html',{
         root: __dirname
     })
 })
-app.get('/about',(req,res) => {
-    res.sendFile('./templates/about.html', {
-        root: __dirname
-    })
-})
-app.get('/contact',(req,res) => {
-    res.sendFile('./templates/contact.html', {
-        root: __dirname
-    })
-})
+
+//app.get('/about',(req,res) => {
+ //   res.sendFile('./templates/about.html', {
+ //       root: __dirname
+ //   })
+//})
+//app.get('/contact',(req,res) => {
+//    res.sendFile('./templates/contact.html', {
+ //       root: __dirname
+ //   })
+//})
 
 app.use((req,res)=>{
     res.status(404).send('No se encontro tu pagina.....!!!!')
